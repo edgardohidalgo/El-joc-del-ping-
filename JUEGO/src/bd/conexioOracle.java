f31cfcd40889412b1faa45226018b88eff1897a0
Edgardo Hidalgo <jugador.eo@gmail.com>
2025-04-10 06:27:46 +0200

    BD PRUBA

---
+++ JUEGO/src/bd/conexioOracle.java
@@ -0,0 +1,32 @@
+package JUEGO.src.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conexioOracle {
+    private Connection conn = null;
+    private String url ,user,pass;
+    public conexioOracle() {
+        conectar();
+    }
+    private void conectar() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            url = "jdbc:oracle:thin:@localhost:1521:XE";
+            user = "scott";
+            pass = "tiger";
+            conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("Se conecta con exito");
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos");;
+        }
+    }
+    public void desconectar() {
+        try{
+            conn.close();
+        }catch (Exception e) {
+            System.out.println("Error al desconectar con la base de datos");;
+        }
+
+    }
+}
