85323537afd933abaa8fb58bd46d81705c7930ea
Edgardo Hidalgo <jugador.eo@gmail.com>
2025-04-11 18:39:45 +0000

    pruebabd

--- JUEGO/src/bd/conexioOracle.java
+++ JUEGO/src/bd/conexioOracle.java
@@ -13,9 +13,9 @@ public class conexioOracle {
     private void conectar() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            url = "jdbc:oracle:thin:@localhost:1521:XE";
-            user = "DM2425_PIN_GRUP07";
-            pass = "AAHRT07";
+            url = "jdbc:oracle:thin:@l";
+            user = "";
+            pass = "";
             conn = DriverManager.getConnection(url, user, pass);
             System.out.println("Se conecta con exito");
         } catch (Exception e) {
