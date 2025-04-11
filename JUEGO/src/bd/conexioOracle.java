336d7e3d37b67641a40af5c8439fb1d81c1e25d6
Edgardo Hidalgo <jugador.eo@gmail.com>
2025-04-11 19:43:26 +0000

    camb
    as

--- JUEGO/src/bd/conexioOracle.java
+++ JUEGO/src/bd/conexioOracle.java
@@ -13,21 +13,24 @@ public class conexioOracle {
     private void conectar() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            url = "jdbc:oracle:thin:@l";
-            user = "";
-            pass = "";
+            url = "jdbc:oracle:thin:@//192.168.3.26:1521/XEPDB2";
+            user = "DM2425_PIN_GRUP07";
+            pass = "AAHRT07 ";
             conn = DriverManager.getConnection(url, user, pass);
-            System.out.println("Se conecta con exito");
+            System.out.println("Se conecto con la base de datos" );;
         } catch (Exception e) {
-            System.out.println("Error al conectar con la base de datos");;
+            System.out.println("Error al desconectar con la base de datos " + e);;
         }
     }
     public void desconectar() {
         try{
             conn.close();
         }catch (Exception e) {
-            System.out.println("Error al desconectar con la base de datos");;
+            System.out.println("Error al desconectar con la base de datos" + e);;
         }
 
+    }
+    public Connection getConn(){
+        return this.conn ;
     }
 }
--- JUEGO/src/bd/mainBD.java
+++ JUEGO/src/bd/mainBD.java
@@ -1,13 +1,14 @@
 package JUEGO.src.bd;
-import bd.conexioOracle;
 public class mainBD {
     
     
     
-    public class MainBD {
+    
         public static void main(String[] args) {
             conexioOracle conn = new conexioOracle(); 
             
-             conn.desconectar();        }
-    }
+            conn.getConn();
+
+        }
+
 }
