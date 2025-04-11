9c8e66edf3287a44c0ccd4d0cb598b6360cc64cd
Edgardo Hidalgo <jugador.eo@gmail.com>
2025-04-11 19:47:32 +0000

    cambiosGenerales

--- JUEGO/src/bd/mainBD.java
+++ JUEGO/src/bd/mainBD.java
@@ -1,9 +1,5 @@
 package JUEGO.src.bd;
 public class mainBD {
-    
-    
-    
-    
         public static void main(String[] args) {
             conexioOracle conn = new conexioOracle(); 
             
